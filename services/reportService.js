843e496817673c7e4d530529e2bc566dfc991ac0
HHJ74 <gpgus0704@gmail.com>
2024-12-27 16:50:48 +0900

    feat : 관리자 관점 신고 접수 및 처리 기능 1차 추가

---
+++ services/reportService.js
@@ -0,0 +1,26 @@
+const reportModel = require('../models/reportModel');
+
+// 도메인별 신고 접수
+exports.createReport = async (domain, reportData) => {
+  return await reportModel.insertReport(domain, reportData);
+};
+
+// 도메인별 신고 조회 (목록)
+exports.getReportsByDomain = async (domain, filters) => {
+  return await reportModel.findReportsByDomain(domain, filters);
+};
+
+// 특정 신고 조회
+exports.getReport = async (domain, reportId) => {
+  return await reportModel.findReportById(domain, reportId);
+};
+
+// 신고 처리 (관리자)
+exports.processReport = async (domain, reportId, processData) => {
+  return await reportModel.updateReportState(domain, reportId, processData);
+};
+
+// 신고 처리 내역 조회
+exports.getReportManagements = async (filters) => {
+  return await reportModel.findReportManagements(filters);
+};
