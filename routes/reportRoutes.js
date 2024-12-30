e5eaf9e74f6febf950d4c0df0a68ac19e88bcff7
HHJ74 <gpgus0704@gmail.com>
2024-12-30 23:37:52 +0900

    feat : 신고 종류 컬럼 추가

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -20,4 +20,4 @@ router.get('/reports/managements', reportController.getReportManagements);
 module.exports = router;
 
 // 신고당한 횟수를 보기위해선 쿼리문의 약간의 추가가 필요 
-// 신고 카테고리를 위해서는s 유저가 신고시 카테고리 저장할 수 있는 칼럼이 필요
+// 신고 카테고리를 위해서는 유저가 신고시 카테고리 저장할 수 있는 칼럼이 필요
