f8404b827b65a5f388333f3a55a246cc01c07db4
sora <ju22hy@gmail.com>
2024-12-31 09:51:22 +0900

    fix(authRoute): 로그인 경로에서 토큰 검증 로직 제거

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,25 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/authController');
-const validation = require('../middlewares/validation');
-const authMiddleware = require('../middlewares/authMiddleware');
+const authController = require("../controllers/authController");
+const validation = require("../middlewares/validation");
+// const authMiddleware = require('../middlewares/authMiddleware');
+
 // 회원가입 엔드포인트
-router.post('/signup', validation.validateSignup, authController.signup);
+router.post("/signup", validation.validateSignup, authController.signup);
 router.post(
-  '/admin-signup',
+  "/admin-signup",
   validation.validateAdminSignup,
   authController.adminsignup
 );
 
 // 로그인 엔드포인트 (user_id로 로그인)
 router.post(
-  '/user-signin',
-  authMiddleware.authenticateToken,
+  "/user-signin",
+  // authMiddleware.authenticateToken,
   authController.login
 );
 router.post(
-  '/admin-signin',
-  authMiddleware.authenticateToken,
+  "/admin-signin",
+  // authMiddleware.authenticateToken,
   authController.adminlogin
 );
 module.exports = router;
