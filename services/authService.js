1edeac43a38ebda72713b8f8c3b1698fced44f74
sora <ju22hy@gmail.com>
2024-12-31 10:42:30 +0900

    fix(authService): 유저 토큰에 user_number 정보 포함

--- services/authService.js
+++ services/authService.js
@@ -1,21 +1,21 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { postgreSQL } = require('../config/database');
-const mongoose = require('mongoose');
-const authModel = require('../models/authModel');
-const userService = require('./userService');
-const config = require('../config/config');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { postgreSQL } = require("../config/database");
+const mongoose = require("mongoose");
+const authModel = require("../models/authModel");
+const userService = require("./userService");
+const config = require("../config/config");
 
 exports.createUser = async (userData) => {
   const client = await postgreSQL.connect(); // PostgreSQL 클라이언트 연결
   const session = await mongoose.startSession(); // MongoDB 세션 시작
 
   try {
-    await client.query('BEGIN'); // PostgreSQL 트랜잭션 시작
+    await client.query("BEGIN"); // PostgreSQL 트랜잭션 시작
     session.startTransaction(); // MongoDB 트랜잭션 시작
 
     const hashedPassword = await bcrypt.hash(userData.user_pw, 10);
-    const sanitizedPhone = userData.user_phone.replace(/\D/g, '');
+    const sanitizedPhone = userData.user_phone.replace(/\D/g, "");
 
     // PostgreSQL에 사용자 생성
     const createdUser = await authModel.createUser({
@@ -38,12 +38,12 @@ exports.createUser = async (userData) => {
       session
     );
 
-    await client.query('COMMIT'); // PostgreSQL 커밋
+    await client.query("COMMIT"); // PostgreSQL 커밋
     await session.commitTransaction(); // MongoDB 커밋
 
     return createdUser; // 생성된 사용자 반환
   } catch (error) {
-    await client.query('ROLLBACK'); // PostgreSQL 롤백
+    await client.query("ROLLBACK"); // PostgreSQL 롤백
     await session.abortTransaction(); // MongoDB 롤백
     throw error;
   } finally {
@@ -57,23 +57,31 @@ exports.loginUser = async (userData) => {
 
   const user = await authModel.findUserById(user_id);
   if (!user) {
-    throw new Error('User not found');
+    throw new Error("User not found");
   }
 
   const isPasswordValid = await bcrypt.compare(user_pw, user.user_pw);
   if (!isPasswordValid) {
-    throw new Error('Invalid password');
+    throw new Error("Invalid password");
   }
 
   const token = jwt.sign(
-    { user_id: user.user_id, user_email: user.user_email },
+    {
+      user_number: user.user_number,
+      user_id: user.user_id,
+      user_email: user.user_email,
+    },
     config.auth.jwtSecret,
     { expiresIn: config.auth.jwtExpiresIn }
   );
 
   return {
     token,
-    user: { user_id: user.user_id, user_email: user.user_email },
+    user: {
+      user_number: user.user_number,
+      user_id: user.user_id,
+      user_email: user.user_email,
+    },
   };
 };
 
@@ -93,12 +101,12 @@ exports.loginAdmin = async (adminData) => {
 
   const admin = await authModel.findAdminById(admin_id);
   if (!admin) {
-    throw new Error('Admin not found');
+    throw new Error("Admin not found");
   }
 
   const isPasswordValid = await bcrypt.compare(admin_pw, admin.admin_pw);
   if (!isPasswordValid) {
-    throw new Error('Invalid password');
+    throw new Error("Invalid password");
   }
 
   const token = jwt.sign(
