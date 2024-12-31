74e9f8fe456c5a35365eff1b0b13dc55bf36c83f
sora <ju22hy@gmail.com>
2024-12-31 11:36:44 +0900

    fix(missionModel): ended_at 데이터 타입 변경

--- services/missionService.js
+++ services/missionService.js
@@ -3,6 +3,10 @@ const missionModel = require("../models/missionModel");
 // 미션 생성 -> 데이터 삽입 실패 시 에러 반환
 exports.createMission = async (missionData, user) => {
   try {
+    // 로그 추가: 전달된 데이터 확인
+    // console.log("[Service] Mission Data:", missionData);
+    // console.log("[Service] User Data:", user);
+
     const mission = await missionModel.createMission(missionData, user);
     return mission;
   } catch (error) {
