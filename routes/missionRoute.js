27612dc9181661803a9678f93ad0cd52a929dc10
sora <ju22hy@gmail.com>
2025-01-07 16:49:25 +0900

    feat: 미션방 생성 시 이미지 업로드 기능 추가중

--- routes/missionRoute.js
+++ routes/missionRoute.js
@@ -1,55 +1,62 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const missionController = require("../controllers/missionController");
-const missionDetailsController = require("../controllers/missionDetailsController");
-const { authenticateToken } = require("../middlewares/authMiddleware");
+const missionController = require('../controllers/missionController');
+const missionDetailsController = require('../controllers/missionDetailsController');
+const { authenticateToken } = require('../middlewares/authMiddleware');
+const { upload, uploadFileToFTP } = require('../middlewares/fileUpload');
 
-// 미션방 생성
-router.post("/", authenticateToken, missionController.createMission);
+// 미션방 생성 (이미지 업로드 추가)
+router.post(
+  '/',
+  authenticateToken, // 사용자 인증
+  upload, // 이미지 업로드 처리 (multer)
+  uploadFileToFTP, // FTP 서버 업로드 및 URL 생성
+  missionController.createMission // 미션방 생성 컨트롤러
+);
 
 // 미션 참여 엔드포인트
 router.post(
-  "/rooms/:room_number/join",
+  '/rooms/:room_number/join',
   authenticateToken,
   missionController.joinMissionRoom
 );
 
 // "지금 주목받는 미션" 5개 조회
-router.get("/popular", missionController.getPopularMissions);
+router.get('/popular', missionController.getPopularMissions);
 
 // 마감 임박 미션 5개 조회
-router.get("/upcoming", missionController.getUpcomingMissions);
+router.get('/upcoming', missionController.getUpcomingMissions);
 
 // 참여 중인 미션 5개 조회
 router.get(
-  "/participating",
+  '/participating',
   authenticateToken,
   missionController.getParticipatingMissions
 );
 
 // 로그인 없이 조회 가능한 미션 전체 조회
-router.get("/popular-all", missionController.getPublicMissions); // 주목받는 미션
-router.get("/upcoming-all", missionController.getPublicMissions); // 마감 임박 미션
+router.get('/popular-all', missionController.getPublicMissions); // 주목받는 미션
+router.get('/upcoming-all', missionController.getPublicMissions); // 마감 임박 미션
 
 // 로그인 필요한 미션 전체 조회
 router.get(
-  "/participating-all",
+  '/participating-all',
   authenticateToken,
   missionController.getAuthRequiredMissions
 ); // 참여 중인 미션
 router.get(
-  "/completed-all",
+  '/completed-all',
   authenticateToken,
   missionController.getAuthRequiredMissions
 ); // 참여했던 미션
 
 // 모집 중인 미션 상세 페이지 조회
 router.get(
-  "/recruiting/:room_number",
+  '/recruiting/:room_number',
   missionDetailsController.getRecruitingMissionDetails
 );
 
 // 진행 중 또는 완료된 미션 상세 페이지 조회
-router.get("/detail/:room_number", missionDetailsController.getMissionDetails);
+router.get('/detail/:room_number', missionDetailsController.getMissionDetails);
 
 module.exports = router;
